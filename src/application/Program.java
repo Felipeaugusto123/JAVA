357b8fb10054a0db5110f8415f9d78002463847f
Felipeaugusto123 <felipeaugustorodriguesmartins@gmail.com>
2024-09-17 23:54:23 -0300

    firsts commit

---
+++ src/application/Program.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+    public static void main(String[] args) {
+        int accountNumber;
+        String name;
+        double value;
+        char response;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account number : ");
+        accountNumber = sc.nextInt();
+        System.out.println("Enter account holder : ");
+        name = sc.next();
+
+        System.out.println("Is there na initial value ? ");
+        response = sc.next().charAt(0);
+
+        if (response == 'y') {
+            System.out.println("Enter initial deposit value : ");
+            value = sc.nextDouble();
+
+            Account account = new Account(accountNumber, name, value);
+            System.out.println("Account data :");
+            System.out.println(account);
+
+            System.out.println("Enter a deposit value : ");
+            value = sc.nextDouble();
+            account.addValueAccount(value);
+
+            System.out.println("Update data : ");
+            System.out.println(account);
+
+            System.out.println("Enter a whitdraw value");
+            value = sc.nextDouble();
+            account.removeValueAccount(value);
+
+            System.out.println("Update data");
+            System.out.println(account);
+
+        } else {
+            Account account = new Account(accountNumber, name);
+            System.out.println("Account data :");
+            System.out.println(account);
+
+            System.out.println("Enter a deposit value : ");
+            value = sc.nextDouble();
+            account.addValueAccount(value);
+
+            System.out.println("Update data : ");
+            System.out.println(account);
+
+            System.out.println("Enter a whitdraw value");
+            value = sc.nextDouble();
+            account.removeValueAccount(value);
+
+            System.out.println("Update data");
+            System.out.println(account);
+
+        }
+
+        sc.close();
+    }
+}
---
+++ src/entities/Account.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class Account {
+    private int numberAccount;
+    public String name;
+    public double value;
+
+    public Account(int numberAccount, String name) {
+        this.numberAccount = numberAccount;
+        this.name = name;
+    }
+
+    public Account(int numberAccount, String name, double value) {
+        this.numberAccount = numberAccount;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getNumberAccount() {
+        return numberAccount;
+    }
+
+    public void setNumberAccount(int numberAccount) {
+        this.numberAccount = numberAccount;
+    }
+
+    public String toString() {
+        return "Account " + numberAccount + ", " + "Holder: " + name + ", " + "Balance " + value;
+    }
+
+
+    public void addValueAccount(double value){
+        this.value += value;
+    }
+
+    public void removeValueAccount(double value){
+       double inicialValue = this.value;
+
+       inicialValue -= value;
+       inicialValue -= 5;
+       
+       this.value = inicialValue;
+    }
+}
